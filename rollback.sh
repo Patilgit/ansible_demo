dd731199033acb3d9319596be129e91fb18d743e
Patilgit <110611110+Patilgit@users.noreply.github.com>
2023-01-09 13:46:29 +0530

    Update rollback.sh

--- rollback.sh
+++ rollback.sh
@@ -1,5 +1,7 @@
+warname=$(ls target/*.war > name | awk -F "/" '{print $2}' name) | sed "s/.war//g"
+response=$(curl --write-out '%{http_code}' --silent  http://65.1.131.188:8080/$warname/ | tail -1)
 if [ $response -eq 200 ];then
-echo "yes"
+exit
 else
-fail
+present deployment failed 
 fi
