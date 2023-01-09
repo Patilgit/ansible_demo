b63e6312dae10e212cf4c85432458ea25b90012a
Patilgit <110611110+Patilgit@users.noreply.github.com>
2023-01-09 13:55:26 +0530

    Update rollback.sh

--- rollback.sh
+++ rollback.sh
@@ -1,5 +1,5 @@
 warname=$(ls target/*.war > name | awk -F "/" '{print $2}' name) | sed "s/.war//g"
-response=$(curl --write-out '%{http_code}' --silent  http://65.1.131.188:8080/$warname/ | tail -1)
+response=$(curl --write-out '%{http_code}' --silent  http://65.1.131.188:8081/$warname/ | tail -1)
 if [ $response -eq 200 ];then
 exit
 else
